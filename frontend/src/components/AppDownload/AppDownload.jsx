6585afc5065fe6da6d1a26c282bee964b119c85f
Ranjith Babu <ranjithbabu0912@gmail.com>
2024-12-05 22:43:27 +0530

    final commit

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 
 const AppDownload = () => {
     return (
-        <div className='app-download' id='app-download'>
+        <div className='app-download' id='download-app'>
             <p>For Better Experience Download <br /> eCaterer App</p>
             <div className="app-download-platforms">
                 <img src={assets.play_store} alt="play store" />
