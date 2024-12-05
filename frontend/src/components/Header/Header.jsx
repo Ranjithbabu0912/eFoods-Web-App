76a2e71f95b512fe579364be6bac006120690147
Ranjith Babu <ranjithbabu0912@gmail.com>
2024-12-05 22:44:49 +0530

    final commit

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
             <div className="header-contents">
                 <h2>Order your favourite food here</h2>
                 <p>Choose from a diverse menu featuring a delecatable array of dishes crafted with the finest ingredients and culinary expertise, Our mission is to satisfy your carvings and elevate your dining experience, one delicious meel at a time.</p>
-                <a href="#explore-menu"><button>View Menu</button></a>
+                <a href="#menu"><button>View Menu</button></a>
             </div>
         </div>
     ) 
